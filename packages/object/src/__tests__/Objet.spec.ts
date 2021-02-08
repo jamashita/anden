b955dd757cc19c309b36ff06b55725fec3e21cc0
jamashita <jamashita@arachnid.xyz>
2021-02-08 22:10:48 +0900

    feat: 🎸 all tests done

--- packages/object/src/__tests__/Objet.spec.ts
+++ packages/object/src/__tests__/Objet.spec.ts
@@ -1,33 +1,6 @@
+import { random, sequence } from '@jamashita/anden-helper';
 import { Inconnu } from '@jamashita/anden-type';
-import { randomBytes } from 'crypto';
 import { Objet } from '../Objet';
-
-const chars: string = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
-
-const sequence = (last: number): Array<number> => {
-  return Array.from(Array(last).keys());
-};
-
-const random = (length: number): Promise<string> => {
-  const charLength: number = chars.length;
-
-  return new Promise((resolve: (value: (string)) => void, reject: (reason?: unknown) => void) => {
-    randomBytes(length, (err: Error | null, buf: Buffer) => {
-      if (err !== null) {
-        reject(err);
-
-        return;
-      }
-
-      const str: string = buf.reduce<string>((p: string, i: number) => {
-        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
-        return `${p}${chars[i % charLength]!}`;
-      }, '');
-
-      resolve(str);
-    });
-  });
-};
 
 describe('Objet', () => {
   describe('identify', () => {
@@ -58,26 +31,24 @@ describe('Objet', () => {
       }
     });
 
-    it('describes string', async () => {
+    it('describes string', () => {
       expect.assertions(100);
 
-      for (let i: number = 0; i < 100; i++) {
-        // eslint-disable-next-line no-await-in-loop
-        const str: string = await random(40);
+      sequence(100).forEach(() => {
+        const str: string = random(40);
 
         expect(Objet.identify(str)).toBe(str);
-      }
+      });
     }, 10_000);
 
-    it('describes symbol', async () => {
+    it('describes symbol', () => {
       expect.assertions(100);
 
-      for (let i: number = 0; i < 100; i++) {
-        // eslint-disable-next-line no-await-in-loop
-        const sym: symbol = Symbol(await random(40));
+      sequence(100).forEach(() => {
+        const sym: symbol = Symbol(random(40));
 
         expect(Objet.identify(sym)).toBe(sym.toString());
-      }
+      });
     }, 10_000);
 
     it('describes bigint', () => {
@@ -88,59 +59,50 @@ describe('Objet', () => {
       }
     });
 
-    it('describes object literal', async () => {
+    it('describes object literal', () => {
       expect.assertions(2);
 
       expect(Objet.identify({})).toBe('[object Object]');
 
       const obj: Inconnu = {};
 
-      const promises: Array<Promise<[string, string]>> = sequence(100).map<Promise<[string, string]>>(async (i: number) => {
-        // eslint-disable-next-line no-await-in-loop
-        return [await random(i), await random(i)];
-      });
-      const kv: Array<[string, string]> = await Promise.all(promises);
-
-      kv.forEach(([key, value]: [string, string]) => {
+      sequence(100).map<[string, string]>((i: number) => {
+        return [random(i), random(i)];
+      }).forEach(([key, value]: [string, string]) => {
         obj[key] = value;
       });
 
       expect(Objet.identify(obj)).toBe('[object Object]');
     }, 10_000);
 
-    it('describes object.create(null)', async () => {
+    it('describes object.create(null)', () => {
       expect.assertions(2);
 
       expect(Objet.identify(Object.create(null))).toBe('[object Object]');
 
       const obj: Inconnu = {};
 
-      const promises: Array<Promise<[string, string]>> = sequence(100).map<Promise<[string, string]>>(async (i: number) => {
-        // eslint-disable-next-line no-await-in-loop
-        return [await random(i), await random(i)];
-      });
-      const kv: Array<[string, string]> = await Promise.all(promises);
-
-      kv.forEach(([key, value]: [string, string]) => {
+      sequence(100).map<[string, string]>((i: number) => {
+        return [random(i), random(i)];
+      }).forEach(([key, value]: [string, string]) => {
         obj[key] = value;
       });
 
       expect(Objet.identify(obj)).toBe('[object Object]');
     }, 10_000);
 
-    it('returns itself when it has toString()', async () => {
+    it('returns itself when it has toString()', () => {
       expect.assertions(100);
 
-      for (let i: number = 0; i < 100; i++) {
-        // eslint-disable-next-line no-await-in-loop
-        const str: string = await random(40);
+      sequence(100).forEach(() => {
+        const str: string = random(40);
 
         expect(Objet.identify({
           toString(): string {
             return str;
           }
         })).toBe(str);
-      }
+      });
     }, 10_000);
   });
 });
