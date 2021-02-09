b8b9d672a1e59ac52f93eca2939a83f38837f8aa
jamashita <jamashita@arachnid.xyz>
2021-02-09 23:54:18 +0900

    feat: 🎸 uuid package release ready

---
+++ packages/uuid/src/UUIDValidationRule.ts
@@ -0,0 +1,14 @@
+import { Kind } from '@jamashita/anden-type';
+import { ValidationError, ValidationRule } from '@jamashita/anden-validation';
+import { UUID } from './UUID';
+
+export class UUIDValidationRule implements ValidationRule {
+  public evaluate(_target: object, value: unknown): void {
+    if (!Kind.isString(value)) {
+      throw new ValidationError('VALUE IS NOT STRING');
+    }
+    if (!UUID.validate(value)) {
+      throw new ValidationError(`THIS STRING IS NOT SUITABLE FOR UUID. GIVEN: ${value}`);
+    }
+  }
+}
