6e71a72c8c47b08166f22c02a881af51a5d6f84f
jamashita <jamashita@arachnid.xyz>
2021-04-26 21:56:47 +0900

    feat: 🎸 lintstaged

--- packages/error/src/DataSourceError.ts
+++ packages/error/src/DataSourceError.ts
@@ -2,11 +2,15 @@ import { RuntimeError } from './RuntimeError';
 
 export class DataSourceError<N extends string = string, S extends string = string> extends RuntimeError<N> {
   public readonly noun: N;
-  public readonly source: S;
+  private readonly source: S;
 
   public constructor(noun: N, source: S, message: string, cause?: Error) {
     super(message, cause);
     this.noun = noun;
     this.source = source;
+  }
+
+  public getSource(): string {
+    return this.source;
   }
 }
