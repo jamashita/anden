0c7e03d85b5cd7021c0ba6d5f36d33488e42e849
jamashita <jamashita@arachnid.xyz>
2021-07-21 07:39:42 +0900

    feat: 🎸 remove commonJS callming method

--- packages/helper/src/Random.ts
+++ packages/helper/src/Random.ts
@@ -1,11 +1,11 @@
-import { randomBytes } from 'crypto';
+import crypto from 'crypto';
 
 const chars: string = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ234567';
 
 export const random = (length: number): string => {
   const charLength: number = chars.length;
 
-  const buf: Buffer = randomBytes(length);
+  const buf: Buffer = crypto.randomBytes(length);
   return buf.reduce<string>((p: string, i: number) => {
     // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
     return `${p}${chars[i % charLength]!}`;
