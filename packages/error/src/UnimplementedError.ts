463d3228c76177a59172b54a8a641d502ba59f80
jamashita <yamashita@arachnid.xyz>
2022-04-13 17:08:34 +0900

    feat: 🎸 modification for exactOptionalPropertyTypes

--- packages/error/src/UnimplementedError.ts
+++ packages/error/src/UnimplementedError.ts
@@ -1,7 +1,8 @@
+import { Ambiguous } from '@jamashita/anden-type/src/index';
 import { RuntimeError } from './RuntimeError';
 
 export class UnimplementedError extends RuntimeError {
-  public constructor(message: string = 'UNIMPLEMENTED', cause?: Error) {
+  public constructor(message: string = 'UNIMPLEMENTED', cause?: Ambiguous<Error>) {
     super(message, cause);
   }
 }
