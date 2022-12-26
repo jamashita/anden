4a4d1272575d1534e1df84db7c5e3ebae41c3fcc
jamashita <jamashita@arachnid.xyz>
2022-12-27 02:27:07 +0900

    feat: 🎸 runtimeerror reverted

--- src/error/RuntimeError.ts
+++ src/error/RuntimeError.ts
@@ -1,42 +1,10 @@
-import { ValueObject } from '../object/index.js';
 import { Ambiguous, Kind } from '../type/index.js';
 
 const SEPARATOR_TEXT: string = '\nCaused by';
 
-export class RuntimeError extends ValueObject implements Error {
-  private readonly error: Error;
-
+export class RuntimeError extends Error {
   public constructor(message: string, cause?: Error) {
-    super();
-
-    if (Kind.isUndefined(cause)) {
-      this.error = new Error(message);
-
-      return;
-    }
-
-    this.error = new Error(message, { cause });
-  }
-
-  public equals(other: unknown): boolean {
-    if (this === other) {
-      return true;
-    }
-    if (!(other instanceof RuntimeError)) {
-      return false;
-    }
-    if (this.error.name !== other.error.name) {
-      return false;
-    }
-    if (this.error.message !== other.error.message) {
-      return false;
-    }
-
-    return true;
-  }
-
-  public serialize(): string {
-    return this.error.message;
+    super(message, { cause });
   }
 
   private stackErrors(error: Error, chain: Array<Error>): void {
@@ -51,22 +19,10 @@ export class RuntimeError extends ValueObject implements Error {
     return this.stack;
   }
 
-  public get [Symbol.toStringTag](): string {
-    return this.constructor.name;
-  }
-
-  public get message(): string {
-    return this.error.message;
-  }
-
-  public get name(): string {
-    return this.constructor.name;
-  }
-
-  public get stack(): string {
+  public override get stack(): string {
     const chain: Array<Error> = [];
 
-    this.stackErrors(this.error, chain);
+    this.stackErrors(this, chain);
 
     return chain.map((err: Error): Ambiguous<string> => {
       return err.stack;
