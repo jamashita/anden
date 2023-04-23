b0aafcdacdcb0fea96f5c4567e1f2432abbd24b6
jamashita <jamashita@arachnid.xyz>
2023-04-23 15:37:29 +0900

    feat: 🎸 validators accept undefined

--- src/type/Decorator/StringValidation.ts
+++ src/type/Decorator/StringValidation.ts
@@ -1,10 +1,11 @@
 import { StringValidationArgs, StringValidationRule } from '../Rules/StringValidationRule.js';
+import { Ambiguous } from '../Value.js';
 import { addRule } from './Validate.js';
 
 export const StringValidation = (args?: StringValidationArgs): ParameterDecorator => {
   const v: StringValidationRule = StringValidationRule.of(args);
 
-  return (target: object, key: string | symbol, index: number) => {
+  return (target: object, key: Ambiguous<string | symbol>, index: number) => {
     addRule(target, key, index, v);
   };
 };
