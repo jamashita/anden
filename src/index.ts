57235f185c1d131907f4b792798ef9ce3d1b9446
jamashita <jamashita@arachnid.xyz>
2023-08-27 17:34:19 +0900

    fix: 🐛 export url

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,6 @@ export * from './helper/index.js';
 export * from './object/index.js';
 export * from './type/index.js';
 export * from './ulid/index.js';
+export * from './url/index.js';
 export * from './uuid/index.js';
 export * from './zeit/index.js';
