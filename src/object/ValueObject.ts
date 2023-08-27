6188269d6a069c8b0c7fd7a65fc5cd315d64a396
jamashita <jamashita@arachnid.xyz>
2023-08-27 15:52:57 +0900

    feat: 🎸 equals() updated

--- src/object/ValueObject.ts
+++ src/object/ValueObject.ts
@@ -1,8 +1,50 @@
-import { Kind } from '../type/index.js';
+import { Equatable, Kind, Primitive } from '../type/index.js';
 import { Objet } from './Objet.js';
 
 export abstract class ValueObject extends Objet {
   protected $$hash?: string;
+
+  protected abstract getEquatableProperties(): Array<Equatable>;
+
+  protected abstract getPrimitiveProperties(): Array<Primitive>;
+
+  public override equals(other: unknown): boolean {
+    if (this === other) {
+      return true;
+    }
+    if (!(other instanceof ValueObject)) {
+      return false;
+    }
+    if (this.constructor.name !== other.constructor.name) {
+      return false;
+    }
+
+    const thisPrimitives: Array<Primitive> = this.getPrimitiveProperties();
+    const otherPrimitives: Array<Primitive> = other.getPrimitiveProperties();
+
+    if (thisPrimitives.length !== otherPrimitives.length) {
+      return false;
+    }
+
+    const totallySame1: boolean = !thisPrimitives.some((v: Primitive, i: number) => {
+      return v !== otherPrimitives[i];
+    });
+
+    if (!totallySame1) {
+      return false;
+    }
+
+    const thisEquatables: Array<Equatable> = this.getEquatableProperties();
+    const otherEquatables: Array<Equatable> = other.getEquatableProperties();
+
+    if (thisEquatables.length !== otherEquatables.length) {
+      return false;
+    }
+
+    return !thisEquatables.some((v: Equatable, i: number) => {
+      return !v.equals(otherEquatables[i]);
+    });
+  }
 
   public override hashCode(): string {
     if (!Kind.isUndefined(this.$$hash)) {
